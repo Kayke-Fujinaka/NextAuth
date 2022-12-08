09623357fc9e38b5151eb639dcfe3a883d5a0383
Kayke-Fujinaka <kaykealvesfujinaka@gmail.com>
2022-12-08 11:40:53 -0300

    feat: create withSSRAuth to validade auth

---
+++ src/utils/withSSRAuth.ts
@@ -0,0 +1,27 @@
+import {
+  GetServerSideProps,
+  GetServerSidePropsContext,
+  GetServerSidePropsResult,
+} from "next";
+import { parseCookies } from "nookies";
+
+export function withSSRAuth<P>(fn: GetServerSideProps<P>): GetServerSideProps {
+  return async (
+    ctx: GetServerSidePropsContext
+  ): Promise<GetServerSidePropsResult<P>> => {
+    const cookies = parseCookies(ctx);
+
+    const hasCookies = cookies["nextauth.token"];
+
+    if (!hasCookies) {
+      return {
+        redirect: {
+          destination: "/dashboard",
+          permanent: false,
+        },
+      };
+    }
+
+    return await fn(ctx);
+  };
+}
