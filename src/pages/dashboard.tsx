c372889569fba98ebbd40264987eb52ea9831f47
Kayke-Fujinaka <kaykealvesfujinaka@gmail.com>
2022-12-12 11:33:08 -0300

    feat: create Can component

--- src/pages/dashboard.tsx
+++ src/pages/dashboard.tsx
@@ -1,21 +1,19 @@
 import { useContext } from "react";
+import { Can } from "../components/Can";
 import { AuthContext } from "../contexts/AuthContext";
-import { useCan } from "../hooks/useCan";
 import { setupAPIClient } from "../services/api";
 import { withSSRAuth } from "../utils/withSSRAuth";
 
 export default function Dashboard({ data }) {
   const { user } = useContext(AuthContext);
 
-  const userCanSeeMetrics = useCan({
-    permissions: ["metrics.list"],
-  });
-
   return (
     <div>
       <h1>Dashboard: {data?.email}</h1>
 
-      {userCanSeeMetrics && <div>Métricas</div>}
+      <Can permissions={["metrics.list"]}>
+        <div>Métricas</div>
+      </Can>
     </div>
   );
 }
